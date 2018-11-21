e2fd18ea55fa7e4f9c3db06f2a4ba90230bbd1cd
JoeLametta <JoeLametta@users.noreply.github.com>
2018-11-21 08:00:00 +0000

    Drop whipper caching [10/?]
    Signed-off-by: JoeLametta <JoeLametta@users.noreply.github.com>

--- whipper/common/directory.py
+++ whipper/common/directory.py
@@ -29,16 +29,6 @@ def config_path():
     return join(path, 'whipper.conf')
 
 
-# FIXME: maybe the cache_path should be dropped
-def cache_path(name=None):
-    path = join(getenv('XDG_CACHE_HOME') or join(expanduser('~'), '.cache'),
-                'whipper')
-    if name:
-        path = join(path, name)
-    makedirs(path, exist_ok=True)
-    return path
-
-
 def data_path(name=None):
     path = join(getenv('XDG_DATA_HOME') or
                 join(expanduser('~'), '.local/share'),
